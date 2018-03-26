8aa772017a954d183bb0bb961ad19bfcc04a8778
Almasov Tagir <tagir-a@Almasovs-MacBook-Pro.local>
2018-03-26 22:12:56 +0300

    fix(training.js): update script

--- scripts/training.js
+++ scripts/training.js
@@ -1,15 +1,14 @@
 (function(){
     document.addEventListener('DOMContentLoaded', function(){
-        function getParameterByName(name) {
-            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-                results = regex.exec(location.search);
-            return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-        }
+        function getParam(name) {
+            const searchString = window.location.search
+            const params = new URLSearchParams(searchString)
+            const param = params.get(name)
+            return param
+          }
         const title = document.querySelector('#title')
-        const searchString = window.location.search
-        const searchParams = new URLSearchParams(searchString)
-        const param = searchParams.get('utm')
+        const param = getParam('utm')
+        console.log(param)
         title.textContent = param == 2 ? 'Стройка в Одинцово' : 'Стройка в Кунцево'
     })
 })()
